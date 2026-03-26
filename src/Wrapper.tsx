e702679fa3c2ef1e3aa3d22208d308c7f34a1450
David Costa <davidcostadev@gmail.com>
2026-03-26 18:00:26 -0300

    refactor: Remove last visited page functionality and useLastVisitedPage hook

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
-import { useLastVisitedPage } from './hooks/useLastVisitedPage';
 
 const Wrapper = () => {
-  const { redirectToLastVisitedPage } = useLastVisitedPage();
-
-  useEffect(() => {
-    redirectToLastVisitedPage();
-  }, [redirectToLastVisitedPage]);
-
   return (
     <HelmetProvider>
       <Outlet />
